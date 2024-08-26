50b66267c8a8abff6690ea98b8b96e303ce40915
hect1x <christian.sdkn@gmail.com>
2024-08-26 17:03:35 +0700

    added LLM feature

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask , render_template, jsonify, request
 import pandas as pd
 import ast, difflib
-import requests, os
+import requests, os, replicate
+from dotenv import load_dotenv
 import base64
 #google colab link to retrieve data processing
 # https://colab.research.google.com/drive/1CnBUjkGxKm2F-vaQBRA6FvpX1mhcD-5n?usp=sharing
+load_dotenv()
 app = Flask(__name__)
 
 os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
+os.environ["REPLICATE_API_TOKEN"] = os.getenv("REPLICATE_API_TOKEN")  # Get from .env
+huggingface_api_token = os.getenv("HUGGINGFACE_API_TOKEN")
 
 final_df = pd.read_csv('final_df.csv')
 
@@ -22,6 +26,10 @@ def index():
 @app.route("/generate")
 def generate():
     return render_template('generate.html')
+
+@app.route("/llmPage")
+def llmPage():
+    return render_template('llmPage.html')
 
 
 @app.route('/recommend', methods=['POST'])
@@ -89,7 +97,7 @@ def generateImage():
     api_url = "https://api-inference.huggingface.co/models/Ojimi/anime-kawai-diffusion"
 
     headers = {
-        "Authorization": "Bearer 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"
+        "Authorization": f"Bearer {huggingface_api_token}"
     }
 
     data = {
@@ -108,6 +116,22 @@ def generateImage():
     else:
         print('Error from Hugging Face API:', response.status_code, response.text)
         return jsonify({"error": "Failed to generate image"}), 500
+
+@app.route('/generate-text', methods= ['POST'])
+def generateText():
+    pre_prompt = "You are a helpful assistant. You specialize in stuff like anime. If 'user' asks for something outside of anime, come up with a topic to go back to anime"
+    prompt = request.form.get('prompt')
+    # print(prompt)
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                        input={"prompt": f"{pre_prompt} {prompt} Assistant: ", 
+                        "temperature":0.1, "top_p":0.9, "max_length":128, "repetition_penalty":1})  
+    # print(output)
+    full_response = ""
+
+    for item in output:
+        full_response += item
+
+    return jsonify({"text" : full_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
